9ddd8c79084ee87100e898757958f411ebec5a09
dhanvi324 <dhanvishah2006@gmail.com>
2026-02-01 19:48:17 +0530

    removed files

--- mainapplication.js
+++
@@ -1,6 +0,0 @@
-import {addTask,getAllTask,completeTask} from './task.js'
-addTask('competeAssignment','high','2026-01-27')
-addTask('returnBook','medium','2026-01-30')
-console.log(getAllTask())
-completeTask(1)
-console.log(getAllTask())
